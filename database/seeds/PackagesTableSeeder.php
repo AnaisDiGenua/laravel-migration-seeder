5f9a2fc46c42474bcdc40e5a70a3c5d907bc5e9c
AnaisDiGenua <anais.digenua5@gmail.com>
2022-02-07 20:23:51 +0100

    create faker

--- database/seeds/PackagesTableSeeder.php
+++ database/seeds/PackagesTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
+use App\Package;
 
 class PackagesTableSeeder extends Seeder
 {
@@ -10,8 +11,16 @@ class PackagesTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i = 0; $i < 100; $i++) {
+            $package = new Package();
+            $package->city = $faker->city();
+            $package->price = $faker->randomFloat(2, 200, 5000);
+            $package->start_day = $faker->dateTimeThisYear();
+            $package->end_day = $faker->dateTimeThisYear();
+            $package->description =  $faker->paragraph(2);
+            $package->save();
+        }
     }
 }
